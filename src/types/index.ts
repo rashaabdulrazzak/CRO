599aa32db92d88e04f13c84336161dc4a30f8cb1
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2025-10-07 16:26:14 +0300

    add roles

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ export const roleOptions = [
   { label: "Monitor", value: "Monitor" },
   { label: "Biostatistician", value: "Biostatistician" },
   { label: "Site coordinator", value: "Site coordinator" },
-  { label: "Hekim", value: "Hekim" },
-  { label: "Physician", value: "Physician" },
-  { label: "Patolog Hekim", value: "Patolog Hekim" },
+  { label: "Radiologist", value: "Radiologist" },
+  { label: "Patolog coordinator", value: "Physician" },
+  { label: "Admin", value: "Admin" },
 ];
